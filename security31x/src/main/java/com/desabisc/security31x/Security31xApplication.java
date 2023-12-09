d641fe165dfab86fe2451a0f0e6546687bf1daa5
abi <desabi@live.com>
2023-12-09 13:21:50 -0600

    feat: add base project

---
+++ security31x/src/main/java/com/desabisc/security31x/Security31xApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.security31x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security31xApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security31xApplication.class, args);
+	}
+
+}
---
+++ security31x/src/test/java/com/desabisc/security31x/Security31xApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.security31x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security31xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
